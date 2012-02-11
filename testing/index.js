2afdf2eb58fea4b742beac4282441a3ff92fb56d
Tenphi <tenphi@gmail.com>
2012-02-11 15:38:15 +0300

    * prefixes fix; camelCase fix

--- testing/index.js
+++ testing/index.js
@@ -1,6 +1,10 @@
 var jss = require('../lib/jss');
 var styles = require('./styles');
 
+jss.handler('borderRadius', function(value) {
+    return jss.joinPrefixes({borderRadius: value}, ['Moz', 'Webkit']);
+});
+
 console.log(jss.render(styles));
 
 console.log(jss.render(styles, true));
